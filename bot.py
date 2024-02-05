5bc2103a686fb6446fe29b78b1b16b5f83713b6f
GentleGiant1998 <143827840+GentleGiant1998@users.noreply.github.com>
2024-02-05 11:15:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('6575657548:AAGKU2_1iFvEzqLsM43wlfjxBZv6_SQMN2o')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Дарова!")
+    bot.reply_to(message, "Привет!")
 
 
 @bot.message_handler(func=lambda message: True)
